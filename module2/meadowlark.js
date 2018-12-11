f616d77124dcf55aa13a86ac5ea332b0a40df124
Sam <spweir@pointpark.edu>
2018-12-10 23:49:39 -0500

    fortune export

--- module2/meadowlark.js
+++ module2/meadowlark.js
@@ -2,6 +2,8 @@ var express = require('express');
 
 var app = express();
 
+var fortune = require('./lib/fortune.js');
+
 var handlebars = require('express3-handlebars')
     .create({ defaultLayout: 'main' });
 app.engine('handlebars', handlebars.engine);
@@ -17,17 +19,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/about', function (req, res) {
-    var randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
-    res.render('about', { fortune: randomFortune });
+    res.render('about', { fortune: fortune.getFortune() });
 });
-
-var fortunes = [
-    "Conquer your fears or they will conquer you.", 
-    "Rivers need springs.", 
-    "Do not fear what you don't know.", 
-    "You will have a pleasant surprise.", 
-    "Whenever possible, keep it simple.",
-];
 
 //404 catch all handler (middleware)
 app.use(function (req, res, next) {
